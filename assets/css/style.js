ac56679374804dea69a07010bc8026b978e0d8d2
sanyalumagbas <sanya.lumagbas@gmail.com>
2022-02-22 16:02:13 +0800

    scroll to bottom + opinion article

--- assets/css/style.js
+++
@@ -1,8 +0,0 @@
-
-let scrollToBottom = document.querySelector('#scroll-to-bottom');
-let pageBottom = document.querySelector('#page-bottom');
-
-scrollToBottom.addEventListener('click', function () {
-
-  pageBottom.scrollIntoView();
-})
