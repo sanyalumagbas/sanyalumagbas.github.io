852f985d69a6b755bbfef4cc7d572f55930f5804
sanyalumagbas <sanya.lumagbas@gmail.com>
2022-02-23 15:01:52 +0800

    + editorial writing practice 2

---
+++ assets/css/practice.js
@@ -0,0 +1,12 @@
+var quotes = [
+
+
+
+
+
+]
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random () * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
